11d8e3566af4f6a0218aa0aa4e6a0e8a3f8b2e9b
ingseo <inseo37@gmail.com>
2023-03-17 03:18:20 +0900

    파일 위치이동

---
+++ src/components/Main/MainMenuPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MainMenuPage() {
+  return (
+    <div>MainMenuPage</div>
+  )
+}
+
+export default MainMenuPage
---
+++ src/pages/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navigation from '../components/Common/Navigation'
+
+function MainPage() {
+    return (
+    <div>
+        <Navigation />
+    </div>
+    )
+}
+
+export default MainPage
