e1e3d853a56900e4dbbd195456a5d69a395355b6
ingseo <inseo37@gmail.com>
2023-03-28 22:55:58 +0900

    eventListCard component 분리

--- src/components/Main/Contents.js
+++ src/components/Main/Contents.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, Route } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import SliderComponent from './SliderComponent';
+import EventListCard from './EventListCard';
 import '../../style/scss/mainpage.scss'
 
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
@@ -36,31 +37,9 @@ function Contents() {
                                         <img src={ require('../../assets/img/main/event_imegeLogo.png') } alt="고디바 로고" /> 
                                     </div>
                                 </div>
-                                <div className="card">    
-                                    <div className="wrap">
-                                        <div className="front">
-                                            <img src={ require('../../assets/img/main/event1.png') } alt="2020 스프링 앙샹뜨 컬렉션" /></div>
-                                        <div className="back">2020 스프링 앙샹뜨 컬렉션<br />Spring Enchanteur Collection
-                                        <span>바로가기</span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="card">
-                                    <div className="wrap">
-                                        <div className="front">
-                                            <img src={ require('../../assets/img/main/event2.png') } alt="고디바 회원만의 특별한 혜택" /></div>
-                                        <div className="back">고디바 회원만의 특별한 혜택<br />GODIVA MAMBERSHIP
-                                        <span>바로가기</span></div>
-                                    </div>
-                                </div>
-                                <div className="card">
-                                    <div className="wrap">
-                                        <div className="front">
-                                            <img src={ require('../../assets/img/main/event3.png') } alt="프리미엄 컵 아이스크림" /></div>
-                                        <div className="back">프리미엄 컵 아이스크림<br />Premium Cup Ice Cream
-                                        <span>바로가기</span></div>
-                                    </div>
-                                </div>
+                                <EventListCard />
+                                <EventListCard />
+                                <EventListCard />
                                 <ul className="mList">   
                                     <li>2020 스프링 앙샹뜨 컬렉션<br />
                                         Spring Enchanteur Collection </li>
---
+++ src/components/Main/EventListCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function EventListCard() {
+  return (
+    <div className="card">    
+        <div className="wrap">
+            <div className="front">
+                <img src={ require('../../assets/img/main/event1.png') } alt="2020 스프링 앙샹뜨 컬렉션" /></div>
+                <div className="back">2020 스프링 앙샹뜨 컬렉션<br />Spring Enchanteur Collection
+                <span>바로가기</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default EventListCard
