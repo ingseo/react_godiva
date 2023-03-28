a522b7212fbf1fc8b08d254adca59619d402c5a4
ingseo <inseo37@gmail.com>
2023-03-28 22:47:11 +0900

    event list 작업

--- src/components/Main/Contents.js
+++ src/components/Main/Contents.js
@@ -10,6 +10,70 @@ function Contents() {
     return (
         <>
             <SliderComponent />
+            <div className="chocoBg">
+                {/* event */}
+                <div className="event">
+                    <div className="container">
+                        <div className="left">
+                        <div className="text">
+                            <div className="point"><span></span></div>
+                            <div className="title">EVENT LIST</div>
+                            <div className="type">Sweet pleasure</div>
+                            </div>
+                            <ul className="list">
+                                <li>2020 스프링 앙샹뜨 컬렉션<br />
+                                    Spring Enchanteur Collection </li>
+                                <li>고디바 회원만의 특별한 혜택<br />
+                                    GODIVA MAMBERSHIP</li>
+                                <li>프리미엄 컵 아이스크림<br />
+                                    Premium Cup Ice Cream</li>
+                            </ul>
+                        </div>
+                        <div className="right">
+                        <div className="cardGroup">
+                                <div className="card logo">
+                                    <div className="wrap logo">
+                                        <img src={ require('../../assets/img/main/event_imegeLogo.png') } alt="고디바 로고" /> 
+                                    </div>
+                                </div>
+                                <div className="card">    
+                                    <div className="wrap">
+                                        <div className="front">
+                                            <img src={ require('../../assets/img/main/event1.png') } alt="2020 스프링 앙샹뜨 컬렉션" /></div>
+                                        <div className="back">2020 스프링 앙샹뜨 컬렉션<br />Spring Enchanteur Collection
+                                        <span>바로가기</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="card">
+                                    <div className="wrap">
+                                        <div className="front">
+                                            <img src={ require('../../assets/img/main/event2.png') } alt="고디바 회원만의 특별한 혜택" /></div>
+                                        <div className="back">고디바 회원만의 특별한 혜택<br />GODIVA MAMBERSHIP
+                                        <span>바로가기</span></div>
+                                    </div>
+                                </div>
+                                <div className="card">
+                                    <div className="wrap">
+                                        <div className="front">
+                                            <img src={ require('../../assets/img/main/event3.png') } alt="프리미엄 컵 아이스크림" /></div>
+                                        <div className="back">프리미엄 컵 아이스크림<br />Premium Cup Ice Cream
+                                        <span>바로가기</span></div>
+                                    </div>
+                                </div>
+                                <ul className="mList">   
+                                    <li>2020 스프링 앙샹뜨 컬렉션<br />
+                                        Spring Enchanteur Collection </li>
+                                    <li>고디바 회원만의 특별한 혜택<br />
+                                        GODIVA MAMBERSHIP</li>
+                                    <li>프리미엄 컵 아이스크림<br />
+                                        Premium Cup Ice Cream</li>
+                                </ul>
+                            </div>
+                        </div>            
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
