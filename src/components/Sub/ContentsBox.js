44b9df4d2ce9b58d271a119ea76e8e0f6cdd10cd
ingseo <inseo37@gmail.com>
2023-04-10 04:13:14 +0900

    godivamenu subpage css작업 추가

--- src/components/Sub/ContentsBox.js
+++ src/components/Sub/ContentsBox.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link, Route } from "react-router-dom";
+import '../../style/scss/text.scss'
 
 export default function ContentsBox(props) {
     return (
         <div className={props.class}>
             <div className="bg"></div>
             <div className="mBg"></div>
-            <div className="subText subText2">
+            <div className="subText">
                 <div className="title">{props.title}</div>
                 <div className="type">{props.type}</div>
                 <div className="point"><span></span></div>
