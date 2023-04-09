4e629b22cca94e67d48d89ed652204b58e184d73
ingseo <inseo37@gmail.com>
2023-04-10 00:38:07 +0900

    sub ContentsBox 작업

---
+++ src/components/Sub/ContentsBox.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link, Route } from "react-router-dom";
+
+export default function ContentsBox(props) {
+    return (
+        <div className={props.class}>
+            <div className="bg"></div>
+            <div className="mBg"></div>
+            <div className="subText subText2">
+                <div className="title">{props.title}</div>
+                <div className="type">{props.type}</div>
+                <div className="point"><span></span></div>
+                <div className="ex">{props.ex}</div>
+                <Link to="/"><div className="deialisBtn">자세히 보기</div></Link>
+            </div>
+        </div>
+    )
+}
