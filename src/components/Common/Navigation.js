a343aa93f59698277ef20067e79bc19390c5f6b2
ingseo <inseo37@gmail.com>
2023-04-12 03:27:21 +0900

    navi 기능추가

--- src/components/Common/Navigation.js
+++ src/components/Common/Navigation.js
@@ -2,14 +2,35 @@ import React, { useState } from 'react'
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import '../../style/scss/navi.scss'
+import styled from 'styled-components'
 
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
+const DropDownContent = styled.div`
+        height: ${props => (props.display ? '100px' : '0')};
+        position: absolute;
+        width: 100%;
+        overflow: hidden;
+        transition: 0.3s;
+    `
+
 function Navigation() {
 
+    //search
     const [isActive, setIsActive] = useState(false);
     const clickSearch = () => {
         setIsActive(!isActive)
+    }
+
+    //menu
+    const [display, setDisplay] = useState([false, false, false, false]);
+    function outMouseDisplay() {
+        setDisplay([false, false, false, false]);
+    }
+    function inMouseDisplay(index) {
+        let newDisplay = [false, false, false, false];
+        newDisplay[index] = true;
+        setDisplay(newDisplay);
     }
 
     return (
@@ -36,53 +57,86 @@ function Navigation() {
                 <div className="naviDown"></div>
                 <div className="menuGroup">
                     <ul className="downMenu">
-                        <li className="list">
+                        <li className="list" 
+                            onMouseEnter={() => inMouseDisplay(1)}
+                            onMouseLeave={() => outMouseDisplay()}
+                        >
                             <Link to="/menu">GODIVA MENU</Link>    
                         </li>
-                        <li className="list">
+                        <li className="list" 
+                            onMouseEnter={() => inMouseDisplay(2)}
+                            onMouseLeave={() => outMouseDisplay()}
+                        >
                             <Link to="/online">ONLINE EXCLUSIVE</Link>   
                         </li>
-                        <li className="list">
+                        <li className="list" 
+                            onMouseEnter={() => inMouseDisplay(3)}
+                            onMouseLeave={() => outMouseDisplay()}
+                        >
                             <Link to="/news">NEWS＆EVENT</Link>  
                         </li>
-                        <li className="list">
+                        <li className="list" 
+                            onMouseEnter={() => inMouseDisplay(4)}
+                            onMouseLeave={() => outMouseDisplay()}
+                        >
                             <Link to="/about">ABOUT GODIVA</Link>
                         </li>
                     </ul>
                     <div className="subMenuGroup">
-                        <ul className="subMenu">
-                            <div className="wrap godivaMenu">
-                            <li><Link to="/menu">초콜릿│Chocolate</Link></li>
-                            <li><Link to="/menu">음료│Beverage</Link></li>
-                            <li><Link to="/menu">아이스크림│Icecream</Link></li>
-                            <li><Link to="/menu">스낵＆디저트│Snack＆Dessert</Link></li>
-                            <li><Link to="/menu">전체상품│Shop All</Link></li>
-                            </div>
-                        </ul>
-
-                        <ul className="subMenu">
-                            <div className="wrap online">
-                            <li><Link to="/online">온라인몰│Online Shop</Link></li>
-                            <li><Link to="/online">시즌상품│Season Products</Link></li>
-                            <li><Link to="/online">컬렉션│collection</Link></li>
-                            <li><Link to="/online">전체상품│Shop All</Link></li>
-                            </div>
-                        </ul>
-
-                        <ul className="subMenu">
-                            <div className="wrap newsEvent">
-                            <li><Link to="/news">미디어│Press＆Magazine</Link></li>
-                            <li><Link to="/news">프로모션│Event</Link></li>
-                            <li><Link to="/news">공지사항│News</Link></li>
-                            </div>
-                        </ul>
-
-                        <ul className="subMenu">
-                            <div className="wrap about">
-                            <li><Link to="/about">고디바 스토리│Our Story</Link></li>
-                            <li><Link to="/about">고디바 매장│Our Stores</Link></li>
-                            </div>
-                        </ul>
+                        <DropDownContent 
+                            display={display[1]}
+                            onMouseEnter={() => inMouseDisplay(1)}
+                            onMouseLeave={() => outMouseDisplay()}
+                        >
+                            <ul className="subMenu">
+                                <div className="wrap godivaMenu">
+                                    <li><Link to="/menu">초콜릿│Chocolate</Link></li>
+                                    <li><Link to="/menu">음료│Beverage</Link></li>
+                                    <li><Link to="/menu">아이스크림│Icecream</Link></li>
+                                    <li><Link to="/menu">스낵＆디저트│Snack＆Dessert</Link></li>
+                                    <li><Link to="/menu">전체상품│Shop All</Link></li>
+                                </div>
+                            </ul>
+                        </DropDownContent>
+                        <DropDownContent 
+                            display={display[2]}
+                            onMouseEnter={() => inMouseDisplay(2)}
+                            onMouseLeave={() => outMouseDisplay()}
+                        >
+                            <ul className="subMenu">
+                                <div className="wrap online">
+                                    <li><Link to="/online">온라인몰│Online Shop</Link></li>
+                                    <li><Link to="/online">시즌상품│Season Products</Link></li>
+                                    <li><Link to="/online">컬렉션│collection</Link></li>
+                                    <li><Link to="/online">전체상품│Shop All</Link></li>
+                                </div>
+                            </ul>
+                        </DropDownContent>
+                        <DropDownContent 
+                            display={display[3]}
+                            onMouseEnter={() => inMouseDisplay(3)}
+                            onMouseLeave={() => outMouseDisplay()}
+                        >
+                            <ul className="subMenu">
+                                <div className="wrap newsEvent">
+                                    <li><Link to="/news">미디어│Press＆Magazine</Link></li>
+                                    <li><Link to="/news">프로모션│Event</Link></li>
+                                    <li><Link to="/news">공지사항│News</Link></li>
+                                </div>
+                            </ul>
+                        </DropDownContent>
+                        <DropDownContent 
+                            display={display[4]}
+                            onMouseEnter={() => inMouseDisplay(4)}
+                            onMouseLeave={() => outMouseDisplay()}
+                        >
+                            <ul className="subMenu">
+                                <div className="wrap about">
+                                    <li><Link to="/about">고디바 스토리│Our Story</Link></li>
+                                    <li><Link to="/about">고디바 매장│Our Stores</Link></li>
+                                </div>
+                            </ul>
+                        </DropDownContent>
                     </div>
                 </div>
             </div>
