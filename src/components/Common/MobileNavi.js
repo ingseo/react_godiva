ee79c5d997a96acb60b013c778d951cfd76f0c28
ingseo <inseo37@gmail.com>
2023-04-12 05:08:54 +0900

    mobile navi 기능 추가

--- src/components/Common/MobileNavi.js
+++ src/components/Common/MobileNavi.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import '../../style/scss/mnavi.scss';
-
+import styled from 'styled-components'
 import { 
     faCartShopping, 
     faEye, 
@@ -11,7 +11,34 @@ import {
     faRightToBracket
 } from '@fortawesome/free-solid-svg-icons';
 
+const SubContent = styled.div`
+        right: ${props => (props.display ? '0 !important' : '-30rem')};
+        transition: 0.3s;
+    `
+
 function MobileNavi() {
+
+    //open toggle
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleOpen = () => {
+        setIsOpen(!isOpen);
+    }
+    const [isClosed, setIsClosed] = useState(false);
+    const close = () => {
+        setIsClosed(false);
+    }
+
+    //sub menu
+    const [display, setDisplay] = useState([false, false, false, false]);
+    function subMenuOpen(index) {
+        let newDisplay = [...display];
+        newDisplay[index] = true;
+        setDisplay(newDisplay);
+    }
+    function subMenuClose() {
+        setDisplay([false, false, false, false]);
+    }
+
     return (
         <div>
             <div className="mNavi">
@@ -25,57 +52,83 @@ function MobileNavi() {
                     <div className="logo">
                     <Link to="/"><img src={ require('../../assets/img/main/logo_footer.png') } alt="고디바 로고" /></Link>
                     </div>
-                    <div className="mBtn">
-                        <div className="menuBtn">
+                </div>
+            </div>
+
+            <div className="mBtn" onClick={close}>
+                        <div className={`menuBtn ${isOpen ? 'active' : ''}`} onClick={toggleOpen}>
                             <span></span>
                             <span></span>
                             <span></span>
                         </div>
                     </div>
-                </div>
-            </div>
 
-            <div className="mMenu">
+            <div className={`mMenu ${isOpen ? 'view' : ''}`}>
                 <div className="mMainMenu">
                     <ul className="downMenu">
-                        <li className="list godivaManu">GODIVA MENU</li>
-                        <li className="list online">ONLINE EXCLUSIVE</li>
-                        <li className="list newsEvent">NEWS＆EVENT</li>
-                        <li className="list about">ABOUT GODIVA</li>
+                        <li className="list godivaManu"
+                            onClick={() => subMenuOpen(1)}
+                        >
+                            GODIVA MENU
+                        </li>
+                        <li className="list online"
+                            onClick={() => subMenuOpen(2)}
+                        >
+                            ONLINE EXCLUSIVE
+                        </li>
+                        <li className="list newsEvent"
+                            onClick={() => subMenuOpen(3)}
+                        >
+                            NEWS＆EVENT
+                        </li>
+                        <li className="list about"
+                            onClick={() => subMenuOpen(4)}
+                        >
+                            ABOUT GODIVA
+                        </li>
                     </ul>
                 </div>
                 <div className="mSubmenu">
-                    <ul className="subMenu">
+                    <SubContent display={display[1]}>
+                        <ul className={`subMenu ${isClosed ? 'view' : ''}`}>
                             <li><Link to="/menu">초콜릿│Chocolate</Link></li>
                             <li><Link to="/menu">음료│Beverage</Link></li>
                             <li><Link to="/menu">아이스크림│Icecream</Link></li>
                             <li><Link to="/menu">스낵＆디저트│Snack＆Dessert</Link></li>
                             <li><Link to="/menu">전체상품│Shop All</Link></li>
                         </ul>
-                    <ul className="subMenu">
+                    </SubContent>
+                    <SubContent display={display[2]}>
+                        <ul className={`subMenu ${isClosed ? 'view' : ''}`}>
                             <li><Link to="/online">온라인몰│Online Shop</Link></li>
                             <li><Link to="/online">시즌상품│Season Products</Link></li>
                             <li><Link to="/online">컬렉션│collection</Link></li>
                             <li><Link to="/online">전체상품│Shop All</Link></li>
                         </ul>
-                    <ul className="subMenu">
+                    </SubContent>
+                    <SubContent display={display[3]}>
+                        <ul className={`subMenu ${isClosed ? 'view' : ''}`}>
                             <li><Link to="/news">미디어│Press＆Magazine</Link></li>
                             <li><Link to="/news">프로모션│Event</Link></li>
                             <li><Link to="/news">공지사항│News</Link></li>
                         </ul>
-                    <ul className="subMenu">
+                    </SubContent>
+                    <SubContent display={display[4]}>
+                        <ul className={`subMenu ${isClosed ? 'view' : ''}`}>
                             <li><Link to="/about">고디바 스토리│Our Story</Link></li>
                             <li><Link to="/about">고디바 매장│Our Stores</Link></li>
                         </ul>
+                    </SubContent>
                 </div>
                 <div className="subBottom">
                     <ul>
-                        <li><FontAwesomeIcon icon={faRotateLeft} className="icon"/><br /> BACK</li>
+                        <li onClick={() => subMenuClose()}><FontAwesomeIcon icon={faRotateLeft} className="icon"/><br /> BACK</li>
                         <li><FontAwesomeIcon icon={faMagnifyingGlass} className="icon"/><br /> SEARCH</li> 
                         <li><FontAwesomeIcon icon={faRightToBracket} className="icon"/><br /> LOGIN</li>
                     </ul>
                 </div>
             </div>
+            <div className={`bgBlack ${isOpen ? 'black' : ''}`}></div>
         </div>
     )
 }
