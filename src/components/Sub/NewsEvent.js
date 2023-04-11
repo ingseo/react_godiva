0a85efa52ed7c62191fad6cb20be12457ad0de9f
ingseo <inseo37@gmail.com>
2023-04-11 09:22:58 +0900

    news list props 추가

--- src/components/Sub/NewsEvent.js
+++ src/components/Sub/NewsEvent.js
@@ -2,6 +2,7 @@ import React from 'react'
 import SubVisual from './SubVisual';
 import NewsList from './NewsList';
 import EventBox from './EventBox';
+import '../../style/scss/newsevent.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
@@ -13,13 +14,49 @@ function NewsEvent() {
     logo: <div className="miniLogo"><img src={ require('../../assets/img/sub_news/new_logo.png') } alt="미니로고" /></div>,
     type: 'Godiva with you',
     subGroup: 
-      <ul className="menuGroup">
+      <ul className="subGroup">
         <li>NEWS</li>
         <li>EVENT</li>
       </ul>,
-    under: <div><img src={ require('../../assets/img/main/under_arrow.png') } alt="아래화살표" /></div>
+    under: <div className='underArrow'><img src={ require('../../assets/img/main/under_arrow.png') } alt="아래화살표" /></div>
   }
 
+  //newsList props
+  const newsList1 = {
+    first: '공지',
+    second: '제 24기 결산공고',
+    thrid: '2020.03.27',
+  }
+  const newsList2 = {
+    first: '공지',
+    second: '[NOTICE] 신종 바이러스 관련 배송 제한 안내 공지 드립니다.',
+    thrid: '2020.03.05',
+  }
+  const newsList3 = {
+    first: '공지',
+    second: '[NOTICE] 고디바 컵 아이스크림 세븐일레븐 입점',
+    thrid: '2020.01.15',
+  }
+  const newsList4 = {
+    first: '공지',
+    second: '[NOTICE] 고디바 매장별 메뉴 이용 안내',
+    thrid: '2020.03.27',
+  }
+  const newsList5 = {
+    first: '84',
+    second: '고디바 시즌한정 ‘치즈 폼 초콜렉사’ 출시',
+    thrid: '2020.05.06',
+  }
+  const newsList6 = {
+    first: '83',
+    second: '고디바 5월 감사의 달 스페셜 혜택 안내',
+    thrid: '2020.04.29',
+  }
+  const newsList7 = {
+    first: '82',
+    second: '5월 황금연휴 배송 안내',
+    thrid: '2020.04.22',
+  }
 
   return (
     <div className='newsEvent'>
@@ -42,13 +79,13 @@ function NewsEvent() {
       <div className='newEventBox'>
         <div className='newsBox'>
           <div className='container'>
-            <NewsList />
-            <NewsList />
-            <NewsList />
-            <NewsList />
-            <NewsList />
-            <NewsList />
-            <NewsList />
+            <NewsList {...newsList1}/>
+            <NewsList {...newsList2}/>
+            <NewsList {...newsList3}/>
+            <NewsList {...newsList4}/>
+            <NewsList {...newsList5}/>
+            <NewsList {...newsList6}/>
+            <NewsList {...newsList7}/>
           </div>
         </div>
         <div className='eventBox'>
